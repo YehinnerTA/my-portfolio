e7c53a68d3a3a720195daf3e6881d60635a6dca5
YehinnerTA <ytorresastorga@gmail.com>
2025-08-07 00:46:44 -0500

    Insert Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './components/Header.jsx'
-import About from './sections/About.jsx'
+import Home from './sections/Home.jsx'
 import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
-    <About />
+    <Home />
     <Footer />
   </StrictMode>,
 )
---
+++ src/sections/Home.jsx
@@ -0,0 +1,31 @@
+import '../styles/container.css';
+import Foto from '../assets/Foto-Perfil.jpg';
+
+function Home() {
+    return (
+        <>
+            <div className='container-home'>
+                <section className="home" id="home">
+                    <div className="left-content">
+                        <div className="message-bubble">
+                            <div className="message-txt">
+                                <p className="text-home">Hola, Yo soy</p>
+                            </div>
+                        </div>
+                        <h1 className="title-home">Ing. Yehinner D. Torres Astorga</h1>
+                        <p className="text-home">Profesional en desarrollo web y diseñador UI/UX</p>
+                        <div className="home-button">
+                            <a className="btn-cv" href='/public/documents/CV - Yehinner.pdf' download>Descargar CV</a>
+                            <button className="btn-port">Mi trabajo</button>
+                        </div>
+                    </div>
+                    <div className="right-content">
+                        <img src={Foto} alt="Foto perfil" className="perfil-profesional" />
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
+
+export default Home
